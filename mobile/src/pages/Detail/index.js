f3d31de4cf6d9b8b7aea6e1e0b2a8506037a3609
Kelvis Borges <kelvisbcc@gmail.com>
2020-03-29 17:28:02 -0300

    Ajuste envio de mensagem para Whatsapp

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -15,7 +15,8 @@ export default function Detail() {
 
   const incident = route.params.incident;
   const message = `Olá ${incident.name}, estou entrando em contato pois gostaria de ajudar no caso "${incident.title}" com o valor de ${Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL" }).format(incident.value)}`;
-  
+  const messageWhatsApp = `Olá *${incident.name}*, estou entrando em contato pois gostaria de ajudar no caso *${incident.title}* com o valor de *${Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL" }).format(incident.value)}*`;
+
   function navigateBack() {
     navigation.goBack();
   }
@@ -29,7 +30,7 @@ export default function Detail() {
   }
 
   function sendWhatsApp() {
-    Linking.openURL(`whatsapp://send?phone=${incident.whatsapp}&text=${message}`);
+    Linking.openURL(`whatsapp://send?phone=${incident.whatsapp}&text=${messageWhatsApp}`);
   }
 
   return (
